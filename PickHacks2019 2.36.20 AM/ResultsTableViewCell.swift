0d529bb07489ffaef6c8a9f4b13cc822a18843dc
Juila Li <lijulia@wustl.edu>
2020-03-30 16:41:29 -0500

    commit project

---
+++ PickHacks2019 2.36.20 AM/ResultsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsTableViewCell.swift
+//  PickHacks2019
+//
+//  Created by Julia Li on 3/2/19.
+//  Copyright © 2019 STSAdmin. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
